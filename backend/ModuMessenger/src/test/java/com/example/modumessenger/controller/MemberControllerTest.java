e53853910cc06c254ca0f91bf279e9aac6926847
tear94fall <tear94fall@naver.com>
2022-04-10 23:56:05 +0900

    init backend

---
+++ backend/ModuMessenger/src/test/java/com/example/modumessenger/controller/MemberControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.modumessenger.controller;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+class MemberControllerTest {
+
+}

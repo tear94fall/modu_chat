5a9629bc319fea97945d2cbc3ef6ec9bda4afbe0
tear94fall <tear94fall@naver.com>
2022-05-09 22:30:20 +0900

    Feat: "채팅방 관련 api 추가"
    채팅방 관련 api 추가
    
    Resolves: N/A
    Ref: #27
    Related to: #27

--- backend/ModuMessenger/src/main/java/com/example/modumessenger/chat/dto/ChatRoomDto.java
+++ backend/ModuMessenger/src/main/java/com/example/modumessenger/chat/dto/ChatRoomDto.java
@@ -1,4 +1,13 @@
 package com.example.modumessenger.chat.dto;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ChatRoomDto {
+    private String roomId;
+    private String roomName;
+    private String roomImage;
+    private String lastChatMsg;
+    private LocalDateTime lastChatTime;
+    private List<Long> userIds;
 }

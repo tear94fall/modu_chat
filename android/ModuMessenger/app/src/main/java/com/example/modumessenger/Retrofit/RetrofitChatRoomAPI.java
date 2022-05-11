d2732de7ff4efb8b4f34561774279fb2100d8abe
tear94fall <tear94fall@naver.com>
2022-05-11 22:10:53 +0900

    Feat: "채팅방 관련 요청 추가"
    채팅방 생성 요청, 채팅방 정보 가져오기 요청
    
    Resolves: N/A
    Ref: #28, #29, #20
    Related to: #28, #29, #30

--- android/ModuMessenger/app/src/main/java/com/example/modumessenger/Retrofit/RetrofitChatRoomAPI.java
+++ android/ModuMessenger/app/src/main/java/com/example/modumessenger/Retrofit/RetrofitChatRoomAPI.java
@@ -1,15 +1,24 @@
 package com.example.modumessenger.Retrofit;
 
 import com.example.modumessenger.dto.ChatRoomDto;
+import com.example.modumessenger.dto.MemberDto;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RetrofitChatRoomAPI {
 
     @GET("chat/{userId}/rooms")
     Call<List<ChatRoomDto>> RequestChatRooms(@Path("userId") String userId);
+
+    @GET("chat/{roomId}/room")
+    Call<ChatRoomDto> RequestChatRoom(@Path("roomId") String roomId);
+
+    @POST("chat/chat/room")
+    Call<ChatRoomDto> RequestCreateChatRoom(@Body List<String> userIds);
 }

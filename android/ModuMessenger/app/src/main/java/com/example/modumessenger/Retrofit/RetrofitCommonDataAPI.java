565cc8b9b3290e04d7b5cdd85a903d20214470cc
tear94fall <tear94fall@naver.com>
2022-06-11 22:56:10 +0900

    Mod: "Common data retrofit 수정"
    Common data retrofit 수정
    path variable 추가
    
    Resolves: N/a
    Ref: #71, #74
    Related to: #71, #74

--- android/ModuMessenger/app/src/main/java/com/example/modumessenger/Retrofit/RetrofitCommonDataAPI.java
+++ android/ModuMessenger/app/src/main/java/com/example/modumessenger/Retrofit/RetrofitCommonDataAPI.java
@@ -1,10 +1,15 @@
 package com.example.modumessenger.Retrofit;
 
+import com.example.modumessenger.dto.ChatRoomDto;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface RetrofitCommonDataAPI {
 
-    @GET("common/version")
-    Call<CommonData> RequestAppVersion();
+    @GET("common/{key}")
+    Call<CommonData> RequestCommonData(@Path("key") String key);
 }

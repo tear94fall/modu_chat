30b8acf314e760e74657a696424722b5e291496b
tear94fall <tear94fall@naver.com>
2022-06-17 20:43:07 +0900

    Refactor: "이미지 가져오기 api 추가"
    이미지 가져오기 api 추가
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- backend/ModuMessenger/src/main/java/com/example/modumessenger/picture/controller/PictureController.java
+++ backend/ModuMessenger/src/main/java/com/example/modumessenger/picture/controller/PictureController.java
@@ -1,21 +1,30 @@
 package com.example.modumessenger.picture.controller;
 
+import com.example.modumessenger.auth.JwtFactory;
 import com.example.modumessenger.common.hash.SHA256;
 import com.example.modumessenger.picture.dto.PictureDto;
 import com.example.modumessenger.picture.entity.Picture;
 import com.example.modumessenger.picture.service.PictureService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.File;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class PictureController {
 
+    private static Logger log = LoggerFactory.getLogger(PictureController.class);
     private final PictureService pictureService;
 
     @PostMapping("/picture")
@@ -50,5 +59,26 @@ public class PictureController {
         }
 
         return ResponseEntity.ok().body(null);
+    }
+
+    @GetMapping("/picture/{uploader}/{filename:.+}")
+    public ResponseEntity<Resource> displayImage(@PathVariable String filename, @PathVariable String uploader, HttpServletRequest request) {
+        Resource resource = pictureService.loadFileAsResource(uploader, filename);
+
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            log.info("Could not determine file type.");
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }

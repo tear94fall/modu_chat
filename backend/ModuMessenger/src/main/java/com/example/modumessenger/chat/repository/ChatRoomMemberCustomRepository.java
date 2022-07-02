59cc28b3cfcb943d09aa9e8ba566e753113e6a8c
tear94fall <tear94fall@naver.com>
2022-07-02 20:17:28 +0900

    Mod: "채팅방 나기기 api 수정"
    채팅방 나가기 api 수정
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- backend/ModuMessenger/src/main/java/com/example/modumessenger/chat/repository/ChatRoomMemberCustomRepository.java
+++ backend/ModuMessenger/src/main/java/com/example/modumessenger/chat/repository/ChatRoomMemberCustomRepository.java
@@ -4,6 +4,8 @@ import com.example.modumessenger.chat.entity.ChatRoomMember;
 
 public interface ChatRoomMemberCustomRepository {
 
+    ChatRoomMember findByUserIdAndRoomId(String userId, String roomId);
+
     ChatRoomMember findByMemberUserId(String userId);
 
     ChatRoomMember findByRoomByRoomId(String roomId);

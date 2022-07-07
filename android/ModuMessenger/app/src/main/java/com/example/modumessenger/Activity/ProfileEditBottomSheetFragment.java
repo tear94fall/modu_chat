00f566a2a040320f32a5fcdb87e2048af6048253
tear94fall <tear94fall@naver.com>
2022-07-08 05:11:58 +0900

    Feat: "채팅 - 이미지 보내기 추가"
    채팅 - 이미지 보내기 기능 추가
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- android/ModuMessenger/app/src/main/java/com/example/modumessenger/Activity/ProfileEditBottomSheetFragment.java
+++ android/ModuMessenger/app/src/main/java/com/example/modumessenger/Activity/ProfileEditBottomSheetFragment.java
@@ -1,11 +1,9 @@
 package com.example.modumessenger.Activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;

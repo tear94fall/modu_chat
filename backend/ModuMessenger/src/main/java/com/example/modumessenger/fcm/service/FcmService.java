dac846f977f27437c0f0201f0a394bdfc083fc29
tear94fall <tear94fall@naver.com>
2022-07-26 00:22:18 +0900

    Feat: "푸쉬 알림(fcm) 전송 api 추가"
    푸쉬 알림(fcm) 전송 api 추가
    - token 으로 특정 사용자의 기기에만 보내기
    - 전체 유저에게 보내기
    - topic으로 보내기 (topic 구독중인 유저 모두에게 전송)
    
    Resolves: #140, #141, #142, #143
    Ref: #140, #141, #142, #143
    Related to: #140, #141, #142, #143

--- backend/ModuMessenger/src/main/java/com/example/modumessenger/fcm/service/FcmService.java
+++ backend/ModuMessenger/src/main/java/com/example/modumessenger/fcm/service/FcmService.java
@@ -2,9 +2,12 @@ package com.example.modumessenger.fcm.service;
 
 import com.example.modumessenger.fcm.entity.FcmToken;
 import com.example.modumessenger.fcm.repository.FcmRepository;
+import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -15,5 +18,41 @@ public class FcmService {
 
     public FcmToken saveFcmToken(FcmToken fcmToken) {
         return fcmRepository.save(fcmToken);
+    }
+
+    public FcmToken searchFcmToken(String userId) {
+        return fcmRepository.findByUserId(userId);
+    }
+
+    public List<FcmToken> searchAllFcmToken() {
+        return fcmRepository.findAll();
+    }
+
+    public void sendTargetMessage(String targetToken, String title, String body) throws FirebaseMessagingException {
+        this.sendTargetMessage(targetToken, title, body, null);
+    }
+
+    public void sendTargetMessage(String targetToken, String title, String body, String image) throws FirebaseMessagingException {
+        Notification notification = Notification.builder().setTitle(title).setBody(body).setImage(image).build();
+        Message msg = Message.builder().setToken(targetToken).setNotification(notification).build();
+        sendMessage(msg);
+    }
+
+    public void sendTopicMessage(String topic, String title, String body) throws FirebaseMessagingException {
+        this.sendTopicMessage(topic, title, body, null);
+    }
+
+    public void sendTopicMessage(String topic, String title, String body, String image) throws FirebaseMessagingException {
+        Notification notification = Notification.builder().setTitle(title).setBody(body).setImage(image).build();
+        Message msg = Message.builder().setTopic(topic).setNotification(notification).build();
+        sendMessage(msg);
+    }
+
+    public void sendMessage(Message message) throws FirebaseMessagingException {
+        FirebaseMessaging.getInstance().send(message);
+    }
+
+    public void sendMessage(MulticastMessage message) throws FirebaseMessagingException {
+        FirebaseMessaging.getInstance().sendMulticast(message);
     }
 }

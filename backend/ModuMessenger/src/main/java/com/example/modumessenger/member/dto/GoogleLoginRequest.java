a29cca2cb99f1d04155354e58e01f2d1ad27482a
tear94fall <tear94fall@naver.com>
2023-01-17 23:54:36 +0900

    Feat: "구글 OAuth 로그인시 id_token 검증 로직 추가"
    구글 OAuth 로그인시 id_token 검증 로직 추가
    - singup api 수정
    - 안드로이드에서 전달한 id_token을 GoogleIdTokenVerifier 으로 검증
    - 토큰이 변조되지 않았을 경우 정상 처리하도록 수정
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- backend/ModuMessenger/src/main/java/com/example/modumessenger/member/dto/GoogleLoginRequest.java
+++ backend/ModuMessenger/src/main/java/com/example/modumessenger/member/dto/GoogleLoginRequest.java
@@ -1,2 +1,14 @@
-package com.example.modumessenger.member.dto;public class GoogleLoginRequest {
+package com.example.modumessenger.member.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class GoogleLoginRequest {
+
+    private String authType;
+    private String idToken;
 }

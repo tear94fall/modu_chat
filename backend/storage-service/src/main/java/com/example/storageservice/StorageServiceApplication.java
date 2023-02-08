46904f46ad0a68665706c48a5e44d3dcae3d5869
tear94fall <tear94fall@naver.com>
2023-02-09 00:52:33 +0900

    Feat: "Spring cloude config-service 추가"
    Spring cloud config-service 추가
    - 깃허브 저장소로 부터 섫정 가져오도록 경로 추가
    storage-service 추가
    - 설정 가져오는 테스트 api 추가
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

---
+++ backend/storage-service/src/main/java/com/example/storageservice/StorageServiceApplication.java
@@ -0,0 +1,23 @@
+package com.example.storageservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class StorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageServiceApplication.class, args);
+	}
+
+	@GetMapping("/config")
+	public String string(@Value("${message.owner}") String messageOwner, @Value("${message.content}") String messageContent) {
+		return messageOwner + ", " + messageContent;
+	}
+}

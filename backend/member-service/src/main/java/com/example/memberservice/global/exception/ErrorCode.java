ed07cb0aa92a8180b87c3e2379d592fb35762c43
tear94fall <tear94fall@naver.com>
2023-07-14 00:29:03 +0900

    Refactor: "엔티티 setter 제거 및 서비스 리팩토링"
    엔티티 setter 제거
    - setter 어노테이션 제거
    - 생성자 및 업데이트 메소드 추가
    
    멤버 서비스 리팩토링
    - 엔티티 setter 제거에 따른 코드 리팩토링
    - setter 코드 제거 및 업데이트 생성자 또는 업데이트 메소드 사용
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- backend/member-service/src/main/java/com/example/memberservice/global/exception/ErrorCode.java
+++ backend/member-service/src/main/java/com/example/memberservice/global/exception/ErrorCode.java
@@ -20,16 +20,24 @@ public enum ErrorCode {
     EMAIL_NOT_FOUND(BAD_REQUEST, "email을 찾을 수 없습니다."),
     USERID_NOT_FOUND(BAD_REQUEST, "해당 사용자를 찾을 수 없습니다."),
     INVALID_CHAT_ROOM_MEMBER(BAD_REQUEST, "채팅방 멤버가 아닙니다."),
+    ALREADY_EXIST_USERID(BAD_REQUEST, "이미 존재하는 유저의 email 입니다."),
+    CREATE_NEW_USER_FAIL(BAD_REQUEST, "계정 생성에 실패하였습니다."),
 
     // 401
     UNAUTHORIZED_TOKEN_ERROR(UNAUTHORIZED, "인증되지 않은 토큰 입니다."),
+    UNAUTHORIZED_GOOGLE_ID_TOKEN_ERROR(UNAUTHORIZED, "인증되지 않은 구글 id 토큰 입니다."),
 
     // 404
     USERID_NOT_FOUND_ERROR(NOT_FOUND, "해당 사용자 아이디를 찾을수 없습니다."),
     CHAT_NOT_FOUND_ERROR(NOT_FOUND, "채팅을 찾을수 없습니다."),
     CHATROOM_NOT_FOUND_ERROR(NOT_FOUND, "채팅방을 찾을수 없습니다."),
     USERID_FRIENDS_NOT_FOUND_ERROR(NOT_FOUND, "회원님의 아이디로 등록된 친구를 찾을 수 없습니다."),
-    USEREMAIL_FRIENDS_NOT_FOUND_ERROR(NOT_FOUND, "회원님의 메일로 등록된 친구를 찾을 수 없습니다.");
+    USER_EMAIL_FRIENDS_NOT_FOUND_ERROR(NOT_FOUND, "회원님의 메일로 등록된 친구를 찾을 수 없습니다."),
+
+    // 500
+    USER_INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, "사용자 계정 생성에 실패하였습니다."),
+    USER_PROFILE_IMAGE_UPLOAD_ERROR(INTERNAL_SERVER_ERROR, "사용자 프로필 이미지 업로드에 실패했습니다."),
+    USER_WALLPAPER_IMAGE_UPLOAD_ERROR(INTERNAL_SERVER_ERROR, "사용자 배경 이미지 업로드에 실패했습니다.");
 
     private final HttpStatus status;
     private final String message;

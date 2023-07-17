e7f635c862d048ae5a13359336339dfda04ace56
tear94fall <tear94fall@naver.com>
2023-07-17 19:58:45 +0900

    Feat: "프로필 서비스 수정"
    프필 서비스 수정
    - 오프셋으로 조회하는 기능 추가
    - 프로필 단건 조회 기능 추가
    - 프로필 DTO 수정
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- backend/profile-service/src/main/java/com/example/profileservice/profile/repository/ProfileRepositoryImpl.java
+++ backend/profile-service/src/main/java/com/example/profileservice/profile/repository/ProfileRepositoryImpl.java
@@ -1,7 +1,10 @@
 package com.example.profileservice.profile.repository;
 
+import com.example.profileservice.profile.entity.Profile;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 import static com.example.profileservice.profile.entity.QProfile.*;
 
@@ -11,11 +14,31 @@ public class ProfileRepositoryImpl implements ProfileCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Long deleteByMemberProfile(Long memberId, String value) {
+    public Profile findByMemberProfile(Long memberId, Long id) {
+        return queryFactory
+                .selectFrom(profile)
+                .where(profile.memberId.eq(memberId)
+                        .and(profile.id.eq(id)))
+                .fetchOne();
+    }
+
+    @Override
+    public List<Profile> findByMemberProfileOffset(Long memberId, Long id, Long count) {
+        return queryFactory
+                .selectFrom(profile)
+                .where(profile.memberId.eq(memberId)
+                        .and(profile.id.lt(id)))
+                .orderBy(profile.createdDate.desc())
+                .limit(count)
+                .fetch();
+    }
+
+    @Override
+    public Long deleteByMemberProfile(Long memberId, Long id) {
         return queryFactory
                 .delete(profile)
                 .where(profile.memberId.eq(memberId)
-                        .and(profile.value.eq(value)))
+                        .and(profile.id.eq(id)))
                 .execute();
     }
 }

088617d6a212b85001f3aac5981aa962b0756d2f
tear94fall <tear94fall@naver.com>
2023-08-15 23:45:03 +0900

    Mod: "프로필 추가 관련 로직 수정"
    프로필 추가 관련 로직 수정
    - 파일을 업로드 하는 경우와 텍스트 수정 구분하여 업데이트
    - 프로필 서비스에 신규 프로필 추가하는 Retrofit 추가
    - 프로필 생성 Dto 추가
    
    Resolves: N/A
    Ref: N/A
    Related to: N/A

--- android/ModuMessenger/app/src/main/java/com/example/modumessenger/dto/ProfileDto.java
+++ android/ModuMessenger/app/src/main/java/com/example/modumessenger/dto/ProfileDto.java
@@ -8,25 +8,38 @@ import java.time.format.DateTimeFormatter;
 
 public class ProfileDto {
     private Long id;
+    private Long memberId;
     private ProfileType profileType;
     private String value;
     private String createdDate;
     private String updatedDate;
 
     public Long getId() { return this.id; }
+    public Long getMemberId() { return this.memberId; }
     public ProfileType getProfileType() { return this.profileType; }
     public String getValue() { return this.value; }
     public String getCreatedDate() { return this.createdDate; }
     public String getUpdatedDate() { return this.updatedDate; }
 
     public void setId(Long id) { this.id = id; }
+    public void setMemberId(Long memberId) { this.memberId = memberId; }
     public void setValue(String value) { this.value = value; }
     public void setProfileType(ProfileType profileType) { this.profileType = profileType; }
     public void setCreatedDate(String createdDate) { this.createdDate = createdDate; }
     public void setUpdatedDate(String updatedDate) { this.updatedDate = updatedDate; }
 
+    public ProfileDto(Long id, Long memberId, ProfileType type, String value, String createdDate, String updatedDate) {
+        setId(id);
+        setMemberId(memberId);
+        setProfileType(type);
+        setValue(value);
+        if (!createdDate.equals("")) { setCreatedDate(createdDate); }
+        if (!updatedDate.equals("")) { setUpdatedDate(updatedDate); }
+    }
+
     public ProfileDto(Profile profile) {
         setId(profile.getId());
+        setMemberId(profile.getMemberId());
         setProfileType(profile.getProfileType());
         setValue(profile.getValue());
         setCreatedDate(profile.getCreatedDate() != null ? profile.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) : "");
